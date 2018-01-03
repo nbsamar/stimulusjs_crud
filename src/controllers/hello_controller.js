5b2a0986ea999a2237cfde782e8b841d9aa3e55d
Nayab Samar <nbsamar@gmail.com>
2018-01-03 16:27:17 +0530

    Hello! World

--- src/controllers/hello_controller.js
+++ src/controllers/hello_controller.js
@@ -1,5 +1,9 @@
-import { Controller } from "stimulus"
+import {Controller} from "stimulus"
 
 export default class extends Controller {
-
+    connect() {
+        const target_element = this.targets.find("name")
+        const name = target_element.value
+        console.log(`Hey! ${name}`, this.element)
+    }
 }
